91c56c2b523d943ac1e2ca54102bd8f93a1f37d3
Matthew Isabel <matthew.isabel@gmail.com>
2015-01-14 23:18:23 -0500

    Add multi photo post support and external photo links

--- test/base_spec.js
+++ test/base_spec.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var helper = require('./helper');
 var client = helper.client;
 
@@ -61,6 +62,111 @@ describe('_get', function () {
       this.call.url.should.equal('http://api.tumblr.com/v2/the/path?my=options&api_key=consumer_key');
     });
 
+  });
+
+});
+
+describe('client', function () {
+
+  describe('.photo', function () {
+    var mockReadStreamReturn, mockRequestGetReturn;
+    function generateRandomNumberString () {
+      return Math.floor(Math.random(1000000)*100000).toString();
+    };
+
+    beforeEach(function () {
+      // Request post stub
+      request.post = function (options, callback) {
+        var self = this;
+
+        self.postCall = options;
+        self.formData = {};
+        return {
+          form: function () {
+            return {
+              append: function (appendedField, appendedData) {
+                return self.formData[appendedField] = appendedData;
+              },
+              getHeaders: function () {
+                callback(self);
+              }
+            }
+          },
+          oauth: function () {},
+          headers: {},
+          body: {}
+        };
+      }.bind(this);
+
+      // fs stub
+      mockReadStreamReturn = generateRandomNumberString();
+      fs.createReadStream = function (path) {
+        return mockReadStreamReturn;
+      };
+
+      // Request get stub
+      mockRequestGetReturn = generateRandomNumberString();
+      request.get = function (options, callback) {
+        return mockRequestGetReturn;
+      }.bind(this);
+    });
+
+    it('should make a post of type photo', function () {
+      client.photo('test', {
+        source: 'http://nodejs.org/images/logo.png'
+      }, function (requestData) {
+        requestData.formData.type.should.equal('photo');
+      });
+    });
+
+    it('should resolve blog names properly if .tumblr.com is omitted', function () {
+      client.photo('test', {
+        source: 'http://nodejs.org/images/logo.png'
+      }, function (requestData) {
+        requestData.postCall.url.indexOf('test.tumblr.com').should.not.equal(-1);
+      });
+    });
+
+    it('should make a form request with inputted caption', function () {
+      var randomCaption = generateRandomNumberString();
+      client.photo('test', {
+        caption: randomCaption,
+        source: 'http://nodejs.org/images/logo.png'
+      }, function (requestData) {
+        requestData.formData.caption.should.equal(randomCaption);
+      });
+    });
+
+    it('should pull from the local fs when not a web path', function () {
+      var randomCaption = generateRandomNumberString();
+      client.photo('test', {
+        caption: randomCaption,
+        data: './test.jpg'
+      }, function (requestData) {
+        requestData.formData.data.should.equal(mockReadStreamReturn);
+      });
+    });
+
+    it('should make a request for an image when it\'s a web path', function () {
+      var randomCaption = generateRandomNumberString();
+      client.photo('test', {
+        caption: randomCaption,
+        data: 'http://nodejs.org/images/logo.png'
+      }, function (requestData) {
+        requestData.formData.data.should.equal(mockRequestGetReturn);
+      });
+    });
+
+    it('should retrieve multiple images when an array of data is passed', function () {
+      var randomCaption = generateRandomNumberString();
+      client.photo('test', {
+        caption: randomCaption,
+        data: ['./test.jpg', 'http://nodejs.org/images/logo.png']
+      }, function (requestData) {
+        requestData.formData['data[0]'].should.equal(mockReadStreamReturn);
+        requestData.formData['data[1]'].should.equal(mockRequestGetReturn);
+      });
+    });
   });
 
 });
